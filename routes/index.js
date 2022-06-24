0f80b5c99ee83e4da4c41cfabcbdf711fae47d27
CristianMP04 <christian.marti.pernin@gmail.com>
2022-06-24 20:24:44 +0200

    final ite 3
    final ite 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
+const Movie=require('../models/Movie.model')
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
-
+router.get('/', (req, res, next) => res.render('index.hbs')); // prueba .hbs, si peta borrar extension
+//prueba link
+router.get("/movies", (req, res) => {
+    Movie
+    .find()
+    .then(moviesFromApi => {
+        console.log(moviesFromApi)
+      res.render("movies.hbs",{ moviesFromApi });
+    })
+    .catch(error => console.log(error));
+  });
+  
 module.exports = router;
