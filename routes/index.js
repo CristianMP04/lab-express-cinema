0b2c97bb10b8916dd15ea6ddcb38886926ee1a23
CristianMP04 <christian.marti.pernin@gmail.com>
2022-06-25 14:43:04 +0200

    final lab
    final lab

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+
+
 const Movie=require('../models/Movie.model')
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index.hbs')); // prueba .hbs, si peta borrar extension
@@ -13,5 +15,17 @@ router.get("/movies", (req, res) => {
     })
     .catch(error => console.log(error));
   });
+
+  // detalles pelicula
   
+  router.get("/movies/:id", (req, res) => {
+    let singleMovie = req.params.id;
+    Movie
+    .findById({_id: singleMovie})
+    .then(singleTitle => {
+        res.render("detailsFilm.hbs", {singleTitle});
+    })
+    .catch(error => console.log(error));
+});
+
 module.exports = router;
